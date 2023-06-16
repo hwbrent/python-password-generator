e3ddf144fce1f897c444b872f6c5a3f7e89681d6
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-06-16 03:21:41 +0100

    Refactor

--- password_maker.py
+++ password_maker.py
@@ -1,51 +1,24 @@
-import random
+import string
+import secrets
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-numbers = ["0","1","2","3","4","5","6","7","8","9"]
-symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">",".","?","/","]"]
 
-while True:
-    length = input("Length of password wanted: ")
-    try:
-        bruh = int(length)
-        break
-    except:
-        print("Please enter a NUMBER")
-        continue
+# All the characters that can be used in a password.
+CHARS = string.ascii_letters + string.digits + string.punctuation
 
-def letter_picker():
-    index = random.randrange(0,len(alphabet)-1)
-    letter = alphabet[index]
-    decider = random.randrange(1,3)
-    if decider == 2:
-        return letter.upper()
-    return letter
 
-def number_picker():
-    index = random.randrange(0,len(numbers)-1)
-    number = numbers[index]
-    return number
+def main() -> None:
+    length_raw = input(
+        "Please specify the number of characters you would like the password to contain: "
+    )
+    desired_length = int(length_raw)
 
-def symbol_picker():
-    index = random.randrange(0,len(symbols)-1)
-    symbol = symbols[index]
-    return symbol
-
-def generate():
     password = ""
-    while len(password) < int(length):
-        decider = random.randrange(1,101)
-        if decider % 2 == 0:
-            letter = letter_picker()
-            password += letter
-        if decider % 2 == 1:
-            number = number_picker()
-            password += number
-        if decider % 3 == 0:
-            symbol = symbol_picker()
-            password += symbol
-    print()
-    print(password)
-    print()
 
-generate()
+    while len(password) != desired_length:
+        password += secrets.choice(CHARS)
+
+    print("Your password is:\n\n" + password + "\n\n")
+
+
+if __name__ == "__main__":
+    main()
